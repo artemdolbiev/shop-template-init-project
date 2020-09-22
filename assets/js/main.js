4608ea3f9817b2a2a0cc36286ba66fe1e6095021
Artem Dolbiev <dolbievartem@gmail.com>
2020-09-22 19:12:27 +0300

    Написан скрипт

---
+++ assets/js/main.js
@@ -0,0 +1,39 @@
+let buttonsContainer = document.querySelector('#content-container');
+let cartCounterLabel = document.querySelector('#cart-counter');
+let cartCounter = 0;
+let cartPrice = 0;
+
+let btnClickHandler = (e) => {
+  let target = e.target;
+
+  if (target.classList.contains('item-actions__cart')) {
+
+    cartCounterLabel.innerHTML = `${++cartCounter}`;
+    if (cartCounter === 1) cartCounterLabel.style.display = 'block';
+
+    const mockData = +target.
+      parentElement.
+      previousElementSibling.
+      innerHTML.
+      replace(/^\$(\d+)\s\D+(\d+).*$/u, '$1.$2');
+
+    cartPrice = Math.round((cartPrice + mockData) * 100) / 100;
+    let restoreHTML = target.innerHTML;
+
+    target.innerHTML = `Added ${cartPrice.toFixed(2)} $`;
+
+    console.log(cartPrice);
+
+    buttonsContainer.removeEventListener('click', btnClickHandler);
+    target.disabled = true;
+
+    setTimeout(() => {
+      target.innerHTML = restoreHTML;
+      buttonsContainer.addEventListener('click', btnClickHandler);
+      target.disabled = false;
+    }, 2000);
+  }
+};
+
+buttonsContainer.addEventListener('click', btnClickHandler);
+
